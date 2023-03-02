951a79722807e873c1f1c843507ce3bc8728a569
Kateryna Belchynska <72660010+belchinskaya@users.noreply.github.com>
2023-03-02 13:45:17 +0200

    Added styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,13 @@ import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+// import TrackVisibility from 'react-on-screen';
 
 export const Contact = () => {
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [message, setMessage] = React.useState('');
+  const [status, setStatus] = useState({});
   // eslint-disable-next-line
   const [buttonText, setButtonText] = useState('Send');
 
@@ -27,75 +28,83 @@ export const Contact = () => {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+      // .then(() => alert("Message sent!"))
+      .then(() => setStatus({ success: true, message: 'Message sent! I will contact you soon'}))
+      // .catch((error) => alert(error));
+      .catch((error) => {(
+        setStatus({ success: false, message: 'Something went wrong, please try again later.'})
+      )})
   }
 
   return (
     <section id="contact" className="contact">
       <Container>
         <Row className="align-items-center">
-        <Col size={12} md={12}>
-            <h2>Get In Touch</h2>
-            {/* <iframe
-              id="map"
-              className="map-control"
-              width="100%"
-              height="200px"
-              title="map"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325516.3770455822!2d30.532690549999998!3d50.40203550000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4ee15a4505%3A0x764931d2170146fe!2z0JrQuNGX0LIsIDAyMDAw!5e0!3m2!1suk!2sua!4v1677493573800!5m2!1suk!2sua"
-            /> */}
-          </Col >
-          <Col size={12} md={6}>
-            <img src={contactImg} alt="Contact Us"/>
-          </Col>
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-              <form
-                netlify="true"
-                name="contact"
-                onSubmit={handleSubmit}
-              >
-                <Row>
-                  <Col size={12} sm={6} className="px-1">
-                    <input
-                      value={name}
-                      placeholder="Name"
-                      type="text"
-                      id="name"
-                      name="name"
-                      onChange={(e) => setName(e.target.value)}
-                    />
-                  </Col>
-                  <Col size={12} sm={6} className="px-1">
-                    <input
-                      value={email}
-                      placeholder="Email Address"
-                      type="email"
-                      id="email"
-                      name="email"
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </Col>
-                  <Col size={12} className="px-1">
-                  <textarea
-                    value={message}
-                    rows="6"
-                    placeholder="Message"
-                    id="message"
-                    name="message"
-                    onChange={(e) => setMessage(e.target.value)}
-                  />
-                  <button type="submit"><span>{buttonText}</span></button>
-                  </Col>
-                </Row>
-              </form>
-            </div>}
-          </TrackVisibility>
-        </Col>
-        </Row>
+          <Col size={12} md={12}>
+              <h2>Get In Touch</h2>
+              {/* <iframe
+                id="map"
+                className="map-control"
+                width="100%"
+                height="200px"
+                title="map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d325516.3770455822!2d30.532690549999998!3d50.40203550000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf4ee15a4505%3A0x764931d2170146fe!2z0JrQuNGX0LIsIDAyMDAw!5e0!3m2!1suk!2sua!4v1677493573800!5m2!1suk!2sua"
+              /> */}
+            </Col >
+            <Col size={12} md={6}>
+              <img src={contactImg} alt="Contact Us"/>
+            </Col>
+            <Col size={12} md={6}>
+                <div className="animate__animated animate__fadeIn animate__repeat-1">
+                  <form
+                    netlify="true"
+                    name="contact"
+                    onSubmit={handleSubmit}
+                  >
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          value={name}
+                          placeholder="Name"
+                          type="text"
+                          id="name"
+                          name="name"
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          value={email}
+                          placeholder="Email Address"
+                          type="email"
+                          id="email"
+                          name="email"
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea
+                          value={message}
+                          rows="6"
+                          placeholder="Message"
+                          id="message"
+                          name="message"
+                          onChange={(e) => setMessage(e.target.value)}
+                        />
+                        {
+                          status.message &&
+                          <Col>
+                            <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                          </Col>
+                        }
+                        <button type="submit"><span>{buttonText}</span></button>
+                      </Col>
+                    
+                    </Row>
+                  </form>
+                </div>
+            </Col>
+          </Row>
       </Container>
     </section>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ import shopping from "../assets/img/shopping.png";
 import water from "../assets/img/water.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+// import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
 
@@ -57,48 +57,45 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>I show you to all the big and small websites I have done so far</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    {/* <Nav.Item>
-                      <Nav.Link eventKey="first">Projects</Nav.Link>
-                    </Nav.Item> */}
-                    {/* <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item> */}
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                              />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    {/* <Tab.Pane eventKey="second">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane> */}
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
-            </TrackVisibility>
+            <div className="animate__animated animate__fadeIn animate__repeat-1">
+              <h2>Projects</h2>
+              <p>I show you some websites I have done so far</p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  {/* <Nav.Item>
+                    <Nav.Link eventKey="first">Projects</Nav.Link>
+                  </Nav.Item> */}
+                  {/* <Nav.Item>
+                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                  </Nav.Item> */}
+                </Nav>
+                <Tab.Content id="slideInUp" className="animate__animated animate__slideInUp animate__repeat-1">
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {
+                        projects.map((project, index) => {
+                          return (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          )
+                        })
+                      }
+                    </Row>
+                  </Tab.Pane>
+                  {/* <Tab.Pane eventKey="second">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                  </Tab.Pane> */}
+                </Tab.Content>
+              </Tab.Container>
+            </div>
           </Col>
         </Row>
       </Container>
